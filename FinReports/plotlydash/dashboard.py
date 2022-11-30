262876702b62cf5ded9c1011754daa15871cdb34
SSM0022 <simarjit.s.mann@protonmail.com>
2022-11-29 18:42:25 -0700

    Add enter animation for index template that redirects to embedded dashboard to improve ux

--- FinReports/plotlydash/dashboard.py
+++ FinReports/plotlydash/dashboard.py
@@ -4,21 +4,19 @@ from openbb_terminal.sdk import openbb
 import plotly.graph_objects as go
 from datetime import datetime
 
-
-
 def init_dashboard(flask_app):
     """Create a Plotly Dash dashboard."""
     dash_app = Dash(
         server=flask_app,
         routes_pathname_prefix='/dashboard/',
         external_stylesheets=[
-            '/static/dist/css/styles.css',
+            '../static/style.css',
         ]
     )
     
     df = openbb.stocks.load(
         symbol = 'SPY',
-        start_date = '2022-01-01',
+        start_date = '2022-06-01',
         end_date = '2022-11-01',
         interval = 1440,
         prepost = False,
--- FinReports/routes.py
+++ FinReports/routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template
-from openbb_terminal.sdk import openbb
 
 home_bp = Blueprint('home_bp', __name__)
 
-@home_bp.route('/', methods=('GET', 'POST'))
-def home():    
-    return render_template('index.html')
+@home_bp.route('/')
+def home():
+        return render_template('index.html')
