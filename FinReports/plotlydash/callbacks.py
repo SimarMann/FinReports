4fd7dc086c655db76f90d462d02c14e9d53e5754
SSM0022 <simarjit.s.mann@protonmail.com>
2022-12-27 21:35:01 -0700

    Add ratios table to stockscreen dashboard layout with stock data from openbb
     Price to Earnings
     Price to Sales
     Price to Book
     Book Value
     Debt to Equity
     Debt to Assets

--- FinReports/plotlydash/callbacks.py
+++ FinReports/plotlydash/callbacks.py
@@ -12,6 +12,12 @@ def init_callbacks(dash_app):
         Output(component_id='anre', component_property='children'),
         Output(component_id='anex', component_property='children'),
         Output(component_id='bs', component_property='children'),
+        Output(component_id='pe', component_property='children'),
+        Output(component_id='ps', component_property='children'),
+        Output(component_id='pb', component_property='children'),
+        Output(component_id='bv', component_property='children'),
+        Output(component_id='dteq', component_property='children'),
+        Output(component_id='dtas', component_property='children'),
         Input(component_id='search-input', component_property='value'),
         prevent_initial_call = True
         )
@@ -20,7 +26,7 @@ def init_callbacks(dash_app):
         try:
             dict = plot.fa_overview(input_value)
             ann_exp = plot.fa_income(input_value)
-            return dcc.Graph(id='search_output', figure=plot.ohlc_chart(input_value)), dict['Exchange'], dict['Market capitalization'], dict['Revenue TTM'], ann_exp, dict['Book value']
+            return dcc.Graph(id='search_output', figure=plot.ohlc_chart(input_value)), dict['Exchange'], dict['Market capitalization'], dict['Revenue TTM'], ann_exp, dict['Book value'], dict['PE ratio'], dict['Price to sales ratio TTM'], dict['Price to book ratio'], dict['Book total'], 'N/A', 'N/A'
         except (KeyError, TypeError, ValueError) as error:
             print(error)
       
--- FinReports/plotlydash/layout.py
+++ FinReports/plotlydash/layout.py
@@ -33,22 +33,42 @@ def layout(dash_app):
             dcc.Input(id='search-input', className='form-control me-sm-2', placeholder='Search', type='text', debounce=True),
             html.H5(className='mt-3 mb-3 ms-3', children='StockScreen'),
             html.Div(id='search-output'),
-            html.Div(className='card bg-secondary mt-4 data-card pt-4 px-4', children=[
-                html.H3(className='text-center', children=[
-                    Svg([
-                        Path(d='M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z')
-                        ], width="40", height="40", fill="black", className="bi bi-eyeglasses", viewBox="0 0 16 16"), 
-                    'overview'
-                    ]),
-                html.Hr(),
-                html.Table(className='table table-hover', children=[
-                    html.Tr([html.Th('Index'), html.Td(id='indx')]),
-                    html.Tr([html.Th('Market Cap'), html.Td(id='mcap')]),
-                    html.Tr([html.Th('Annual Revenues'), html.Td(id='anre')]),
-                    html.Tr([html.Th('Annual Expenses'), html.Td(id='anex')]),
-                    html.Tr([html.Th('Book/Share'), html.Td(id='bs')]),
-                    html.Tr([html.Th('Cash/Share'), html.Td('N/A', id='cs')])
+            html.Div(className='d-flex flex-row flex-nowrap', children=[
+                html.Div(className='card bg-secondary mt-4 me-5 data-card pt-4 px-4', children=[
+                    html.Div(className='d-flex flex-row-reverse flex-nowrap justify-content-evenly', children=[
+                        html.H3(className='pt-1', children='overview'),
+                        Svg([
+                            Path(d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z'),
+                            Path(d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z')
+                            ], width="32", height="32", fill="black", className="bi bi-eye", viewBox="0 0 16 16") 
+                        ]),
+                    html.Hr(),
+                    html.Table(className='table table-hover', children=[
+                        html.Tr([html.Th('Index'), html.Td(id='indx')]),
+                        html.Tr([html.Th('Market Cap'), html.Td(id='mcap')]),
+                        html.Tr([html.Th('Annual Revenues'), html.Td(id='anre')]),
+                        html.Tr([html.Th('Annual Expenses'), html.Td(id='anex')]),
+                        html.Tr([html.Th('Book/Share'), html.Td(id='bs')]),
+                        html.Tr([html.Th('Cash/Share'), html.Td('N/A', id='cs')])
+                    ])
+                ]),
+                html.Div(className='card bg-secondary mt-4 data-card pt-4 px-4', children=[
+                    html.Div(className='d-flex flex-row-reverse flex-nowrap justify-content-evenly', children=[
+                        html.H3(className='pt-1', children='ratios'),
+                        Svg([
+                            Path(d='M13.442 2.558a.625.625 0 0 1 0 .884l-10 10a.625.625 0 1 1-.884-.884l10-10a.625.625 0 0 1 .884 0zM4.5 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm7 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z')
+                            ], width="32", height="32", fill="black", className="bi bi-percent", viewBox="0 0 16 16")
+                        ]), 
+                    html.Hr(),
+                    html.Table(className='table table-hover', children=[
+                        html.Tr([html.Th('P/E'), html.Td(id='pe')]),
+                        html.Tr([html.Th('P/S'), html.Td(id='ps')]),
+                        html.Tr([html.Th('P/B'), html.Td(id='pb')]),
+                        html.Tr([html.Th('Book Value'), html.Td(id='bv')]),
+                        html.Tr([html.Th('D/E'), html.Td(id='dteq')]),
+                        html.Tr([html.Th('D/A'), html.Td(id='dtas')])
+                    ])
                 ])
-            ])
+           ])
         ])
     ])
