a601772df6157c54c7122d0b106c29fcfb1b729e
SSM0022 <simarjit.s.mann@protonmail.com>
2023-01-19 16:52:44 -0700

    Protect embedded dash app routes & data from unauthorized access
     use @flask_app.before_request decorator to examine request method and header

--- FinReports/macrodash/dashboard.py
+++ FinReports/macrodash/dashboard.py
@@ -1,6 +1,9 @@
-from dash import Dash
+from dash import Dash, page_registry
 from . import layout
 from . import callbacks
+from flask_login import current_user
+from flask import request, session, redirect, jsonify, flash
+
 
 def init_dashboard(flask_app):
     """Create a Plotly Dash dashboard."""
@@ -13,6 +16,35 @@ def init_dashboard(flask_app):
         ]
     )
     
+    @flask_app.before_request
+    def check_login():
+         if request.method == 'GET':
+             if current_user:
+                 if request.url in ['http://127.0.0.1:5000/login', 'http://127.0.0.1:5000/logout']:
+                     return
+                 if 'Referer' in request.headers:
+                     if request.headers['Referer'] in ['http://127.0.0.1:5000/login', 'http://127.0.0.1:5000/logout']:
+                         return 
+                 if current_user.is_authenticated:
+                     return
+                 else:
+                     for pg in page_registry:
+                         if request.path == page_registry[pg]['path']:
+                             session['url'] = request.url
+             flash('You must be logged in to view that page.')                
+             return redirect('http://127.0.0.1:5000/login')                
+                             
+         else:
+             if current_user:
+                 if current_user.is_authenticated or request.path == '/login':
+                     return
+             if (request.headers['Referer'] in ['http://127.0.0.1:5000/login', 'http://127.0.0.1:5000/logout'] 
+                 and (request.path in ['/_dash-layout', '/_dash-dependencies'] or
+                      (request.json['changedPropIds'] == ["_pages_location.pathname", "_pages_location.search"]
+                        or request.json['changedPropIds'] == ['{"index":"redirectLogin","type":"redirect"}.n_intervals']))):
+                 return
+             return jsonify({'status':'401', 'statusText':'unauthorized access'})
+
     layout.layout(dash_app)
     
     callbacks.init_callbacks(dash_app)
